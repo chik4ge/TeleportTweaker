a26610a76a06778da365df798e08cc72d6189924
chikage1205 <manadora1205@gmail.com>
2021-03-26 22:32:44 +0900

    Initial commit

---
+++ src/main/java/com/chikage/teleporttweaker/TeleportTweaker.java
@@ -0,0 +1,72 @@
+package com.chikage.teleporttweaker;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandManager;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.plugin.PluginManager;
+import org.spongepowered.api.text.Text;
+
+@Plugin(
+        id = "tptweaker",
+        name = "Teleporttweaker"
+)
+public class TeleportTweaker {
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private PluginManager pluginManager;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        PluginContainer plugin = pluginManager.getPlugin("tptweaker").orElse(null);
+        CommandSpec tpCommandSpec = CommandSpec.builder()
+                .description(Text.of("/tp [対象のプレイヤー] <移動先のプレイヤー> または /tp [対象のプレイヤー] [移動先のワールド] <x> <y> <z> [<y軸回転> <x軸回転>]"))
+                .permission("tptweaker.command.tp")
+                .arguments(
+                        GenericArguments.firstParsing(
+                                GenericArguments.seq(
+                                        GenericArguments.optional(GenericArguments.player(Text.of("player"))),
+                                        GenericArguments.onlyOne(GenericArguments.player(Text.of("target")))
+                                ),
+                                GenericArguments.seq(
+                                        GenericArguments.optional(GenericArguments.player(Text.of("player"))),
+                                        GenericArguments.optional(GenericArguments.string(Text.of("world"))),
+                                        GenericArguments.integer(Text.of("x")),
+                                        GenericArguments.integer(Text.of("y")),
+                                        GenericArguments.integer(Text.of("z")),
+                                        GenericArguments.optional(
+                                                GenericArguments.seq(
+
+                                                )
+                                        )
+                                )
+                        )
+
+                )
+                .executor(new tpCommandExecutor())
+                .build();
+
+        CommandSpec allowTpCommandSpec = CommandSpec.builder()
+                .description(Text.of("/allowtp set <true|false> [-p]\n" +
+                        "-p: 設定を継続的に保存\n" +
+                        "/allowtp info [対象のプレイヤー]"))
+                .permission("tptweaker.command.allowtp")
+                .executor(new allowTpCommandExexutor())
+                .build();
+
+        if (plugin != null) {
+            CommandManager commandManager = Sponge.getCommandManager();
+            commandManager.register(plugin, tpCommandSpec, "tp");
+            commandManager.register(plugin, allowTpCommandSpec, "allowtp");
+        }
+    }
+}
